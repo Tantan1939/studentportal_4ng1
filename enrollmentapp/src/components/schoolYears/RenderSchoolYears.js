28a3ef661a184f7f2c06c290e0e60b280caca0fc
Tantan1939 <101438750+Tantan1939@users.noreply.github.com>
2023-04-08 08:30:22 +0800

    Done with charts

--- enrollmentapp/src/components/schoolYears/RenderSchoolYears.js
+++ enrollmentapp/src/components/schoolYears/RenderSchoolYears.js
@@ -13,7 +13,19 @@ const CHART_ACTIONS = {
 function reducer(schoolYear, action){
   switch (action.type) {
     case CHART_ACTIONS.REFRESH_PAGE:
-      return {...schoolYear, sy_data : action.payload.data}
+      return {...schoolYear, sy_data : action.payload.data, current_chartData : action.payload.data.sexs, current_chartName : "Sex"}
+
+    case CHART_ACTIONS.CHANGE_PAGE:
+      return {...schoolYear, sy_data : action.payload.data, current_chartData : action.payload.data.sexs, current_chartName : "Sex"}
+
+    case CHART_ACTIONS.SELECT_SEX:
+      return {...schoolYear, current_chartData : schoolYear.sy_data.sexs, current_chartName : "Sex"}
+
+    case CHART_ACTIONS.SELECT_STRAND:
+      return {...schoolYear, current_chartData : schoolYear.sy_data.strands, current_chartName : "Strand"}
+
+    case CHART_ACTIONS.SELECT_YEARLEVEL:
+      return {...schoolYear, current_chartData : schoolYear.sy_data.yearLevels, current_chartName : "Year level"}
 
     default:
       return schoolYear
@@ -24,15 +36,14 @@ function reducer(schoolYear, action){
 export default function RenderSchoolYears() {
   const [schoolYear, sy_dispatch] = useReducer(reducer, {});
   const [schoolYears, setSchoolYears] = useState([]);
-  const [syPages, setSyPages] = useState([]);
 
   useEffect(()=>{
     get_schoolyears();
   }, []);
 
   useEffect(()=>{
-    if (schoolYears){
-      sy_dispatch({ type : CHART_ACTIONS.REFRESH_PAGE, payload : {data:schoolYears[0]} })
+    if (schoolYears.length > 0){
+      sy_dispatch({ type : CHART_ACTIONS.REFRESH_PAGE, payload : { data : schoolYears[0] } })
     }
   }, [schoolYears]);
 
@@ -46,26 +57,17 @@ export default function RenderSchoolYears() {
     };
   };
 
-  console.log(schoolYear);
-
   function errorHandler(error){
     console.error(error);
-  }
+  };
 
-  const data = [
-    ['Task', 'Hours per Day'],
-    ['Work', 11],
-    ['Eat', 2],
-    ['Commute', 2],
-    ['Watch TV', 2],
-    ['Sleep', 7],
-    ['jakol', 7],
-    ['kantot', 7],
-  ];
+  let render_pages = schoolYears.map((sy, index) => (
+    <button key={index} onClick={()=> sy_dispatch({ type : CHART_ACTIONS.CHANGE_PAGE, payload : { data : sy } })}> {index + 1} </button>
+  ));
 
   const options = {
     is3D: true,
-    title: 'My Daily Activities',
+    title: schoolYear.current_chartData ? schoolYear.current_chartName : "",
     pieSliceText: 'percentage',
     pieSliceTextStyle: {
       color: 'black',
@@ -95,17 +97,38 @@ export default function RenderSchoolYears() {
     <div>
       {schoolYear.sy_data ? (
         <div>
-          <Chart
-            chartType="PieChart"
-            data={schoolYear.sy_data.strands}
-            options={options}
-            graph_id="PieChart"
-            width={'100%'}
-            height={'800px'}
-          />
+          <h4> {schoolYear.sy_data.sy_name} </h4>
+          {schoolYear.current_chartData.length ? (
+            <div>
+              <div>
+                <button onClick={() => sy_dispatch({ type : CHART_ACTIONS.SELECT_SEX })}> Sex </button>
+                <button onClick={() => sy_dispatch({ type : CHART_ACTIONS.SELECT_STRAND })}> Strand </button>
+                <button onClick={() => sy_dispatch({ type : CHART_ACTIONS.SELECT_YEARLEVEL })}> Year level </button>
+              </div>
+
+              <Chart
+                chartType="PieChart"
+                data={schoolYear.current_chartData}
+                options={options}
+                graph_id="PieChart"
+                width={'100%'}
+                height={'800px'}
+              />
+
+            </div>
+          ) : (
+            <div> No data to display... </div>
+          )}
+          {schoolYear.sy_data.can_update && (
+            <div>
+              <button> Update {schoolYear.sy_data.sy_name} </button>
+              <button> Update Period </button>
+            </div>
+          )}
+          {render_pages}
         </div>
       ) : (
-        <div> No data </div>
+        <div> No school year found... </div>
       )}
     </div>
   )
