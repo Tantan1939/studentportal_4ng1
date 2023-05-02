e240eca0cf836d218d5162108e717e62bd6f2296
Tantan1939 <101438750+Tantan1939@users.noreply.github.com>
2023-05-02 18:20:43 +0800

    ~ Good ~

--- studentportal_4ng1/settings.py
+++ studentportal_4ng1/settings.py
@@ -147,8 +147,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = ''
-EMAIL_HOST_PASSWORD = ''  # App password
+EMAIL_HOST_USER = 'newfangledsp@gmail.com'
+EMAIL_HOST_PASSWORD = 'rzbozsrnvawuecrw'  # App password
 
 
 PASSWORD_RESET_TIMEOUT = 86400  # 1 Day
